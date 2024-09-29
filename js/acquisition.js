7915c04796734d7c0b2b6cf0247bddf085fa9ab9
Jesus Martínez-Blanco <jesus.martinez-blanco@flixbus.com>
2024-09-29 11:34:25 +0200

    Add acquisition script

---
+++ js/acquisition.js
@@ -0,0 +1,103 @@
+// Inspired by https://web.dev/serial/
+
+let SAMPLING_MILISECONDS = 1000;
+
+let points = [];
+let lineBuffer = '';
+let latestValue = '';
+
+let appendStream = null;
+
+function isConnected(){
+  return appendStream instanceof WritableStream;
+}
+
+// FUNCTION TO START THE SERIAL PORT AND THE CONTINUOUS UPDATE OF latestValue
+async function getReader() {
+    // Inspired by https://github.com/svendahlstrand/web-serial-api
+
+    // Filter on devices with the Arduino Uno USB Vendor/Product IDs.
+    const filters = [
+        { usbVendorId: 0x2341, usbProductId: 0x0043 },
+        { usbVendorId: 0x2341, usbProductId: 0x0001 }
+    ];
+
+    // Prompt user to select an Arduino Uno device.
+    const port = await navigator.serial.requestPort(
+       { filters }
+    );
+
+    // Wait for the serial port to open.
+    await port.open({ baudRate:  getBaudrate()});
+
+    // Remove the button so that user cannot open the port twice.
+    $('#dropdownBaudRate').addClass('disabled');
+    $('#connectButton').addClass('disabled');
+    $('#connectButton').html(`connected to<br><strong>Vid:</strong> ${port.getInfo().usbVendorId}, <strong>Pid:</strong> ${port.getInfo().usbProductId}`);
+
+    // initPlot();
+
+    appendStream = new WritableStream({
+      write(chunk) {
+        lineBuffer += chunk;
+
+        let lines = lineBuffer.split('\r\n');
+        // console.log(lineBuffer);
+        if (lines.length > 1) {
+          lineBuffer = lines.pop();
+        //   latestValue = parseInt(lines.pop().trim());
+        // console.log(latestValue);
+          latestValue=lines.pop();
+          // if (!latestValue.includes('-- READY --')) {
+            // console.log(latestValue);
+          // }
+        }
+      }
+    });
+
+    port.readable
+      .pipeThrough(new TextDecoderStream())
+      .pipeTo(appendStream);
+
+}
+
+// FUNCTION TO FETCH PERIODICALLY AND PLOT THE latestValue
+function acquire(elapsed) {
+    let checked = document.getElementById("switch").checked;
+    if (checked) {
+        // we cannot read data if it is not connected
+        if (!isConnected()){
+            alert('You must CONNECT TO SERIAL first.')
+            $('#switch').prop('checked', false);
+        } else {
+          let pressureMPa = latestValue;
+          points.push(
+              {
+                  'time [s]': elapsed / 1000,
+                  'serial read': latestValue,
+                  'pressure [MPa]': pressureMPa,
+                  'pressure [kPa]': pressureMPa * 1000,
+                  'pressure [psi]': pressureMPa * 145.038,
+              }
+          );
+          plotPoints(points);
+        }
+    } else if (!checked && points.length > 1) {
+        if (points.slice(-1)[0]['serial read'] != undefined) {
+            points.push(
+                {
+                    'time [s]': elapsed / 1000,
+                    'serial read': undefined,
+                    'pressure [MPa]': undefined,
+                    'pressure [kPa]': undefined,
+                    'pressure [psi]': undefined,
+                }
+            );
+        }
+    }
+}
+
+// EXECUTE THE ACQUISITION IN REGULAR INTERVALS
+const interval = d3.interval(acquire, SAMPLING_MILISECONDS);
+
+
